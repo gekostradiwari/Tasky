e25df2e46ea521307a0b58d4bc0ee59ed401e243
0xRinyTux <pernarinaldo@gmail.com>
2025-11-02 23:47:23 +0100

    Login update
    Sesso is now returned by login

--- code/server/app/repository.py
+++ code/server/app/repository.py
@@ -750,15 +750,19 @@ def handle_login(data):
     tag = decision[0]
     if tag == 'ok_token':
         _, token, tipo = decision
-        # Include department id in response
+        # Include department id and sesso in response
         dept_id = None
+        sesso = None
         try:
             user_wrap = get_user_by_token(token)
             if user_wrap and user_wrap.get('user'):
-                dept_id = user_wrap['user'].get('Dipartimento_id_dipartimento')
+                user = user_wrap['user']
+                dept_id = user.get('Dipartimento_id_dipartimento')
+                sesso = user.get('sesso')
         except Exception:
             dept_id = None
-        return jsonify({"data": {"token": token, "type": tipo, "id_dipartimento": dept_id}, "message": "Login effettuato"}), 200
+            sesso = None
+        return jsonify({"data": {"token": token, "type": tipo, "id_dipartimento": dept_id, "sesso": sesso}, "message": "Login effettuato"}), 200
     
     if tag == 'invalid_token':
         raise AuthException("AUTH_TOKEN_INVALID", "Token non valido", 403)
@@ -768,15 +772,19 @@ def handle_login(data):
     
     if tag == 'ok':
         _, token, tipo = decision
-        # Include department id in response
+        # Include department id and sesso in response
         dept_id = None
+        sesso = None
         try:
             user_wrap = get_user_by_token(token)
             if user_wrap and user_wrap.get('user'):
-                dept_id = user_wrap['user'].get('Dipartimento_id_dipartimento')
+                user = user_wrap['user']
+                dept_id = user.get('Dipartimento_id_dipartimento')
+                sesso = user.get('sesso')
         except Exception:
             dept_id = None
-        return jsonify({"data": {"token": token, "type": tipo, "id_dipartimento": dept_id}, "message": "Login effettuato"}), 200
+            sesso = None
+        return jsonify({"data": {"token": token, "type": tipo, "id_dipartimento": dept_id, "sesso": sesso}, "message": "Login effettuato"}), 200
     
     raise AuthException("INVALID_CREDENTIALS", "Email o password non valide", 401)
 
